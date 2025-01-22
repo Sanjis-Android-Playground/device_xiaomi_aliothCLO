e00f76749b5672ad95935b8b26dfd511812d1d80
tanvirr007 <md.tanvirhasannijum@gmail.com>
2025-01-22 11:21:54 +0000

    Revert "alioth: Import Miui Camera if repo exist"
    This reverts commit 68750f513232140f1430cd34a0219ac61dd80230.

--- device.mk
+++ device.mk
@@ -116,8 +116,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
 
 # Camera
-$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
-
 PRODUCT_VENDOR_PROPERTIES += \
     camera.disable_zsl_mode=true
 

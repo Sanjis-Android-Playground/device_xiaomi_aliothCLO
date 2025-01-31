06b7ba6f7b42a45b339b16c0e107cc0c5ce5fac2
Sanjivns <alshahab109@gmail.com>
2025-01-31 16:23:56 +0000

    alioth: steup singing keys

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+#setup signing keys
+$(call inherit-product, vendor/mine/keys.mk)
+
 # Attestation
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
